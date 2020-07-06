16917941ec5b177a35d1eb1ee6779fd5ebc7e1d3
lukec11 <me@lukec.me>
2020-07-05 23:42:51 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
+
+const wc = new WebClient(process.env.SLACK_TOKEN);
+const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+
+const downvote = async (channel, ts) => {
+    await wc.reactions.add({
+        token: process.env.USER_TOKEN,
+        channel: channel,
+        name: 'downvote',
+        timestamp: ts
+    })
+}
+
+
+slackEvents.on('message', async event => {
+    if (event.text.includes('cln.sh')) {
+        await downvote(
+            event.channel,
+            event.ts
+        )
+    }
+});
+
+slackEvents.start(3000).then(() => {
+    console.log('starting server!');
+})
