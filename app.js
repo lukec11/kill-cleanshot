f4a19ef11f1239f99bc2fce9df5376a45846731a
Faisal Sayed <36627266+faisalsayed10@users.noreply.github.com>
2020-07-07 20:42:07 +0530

    added an event for the text 'scam'
    Hey, I'm just kinda learning, did I do it right?

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const downvote = async (channel, ts) => {
 
 
 slackEvents.on('message', async event => {
-    if (event.text.match(/(?:cln.sh)/gi)) {
+    if (event.text.match(/(?:cln.sh)/gi) || (event.text.match(/(?:scam)/gi)) {
         await downvote(
             event.channel,
             event.ts
